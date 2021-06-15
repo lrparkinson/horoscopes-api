a9a711a7e5b15d75eada46fe5f0532b59acbe852
BBDNET\bbdnet2298 <laurenm@bbd.co.za>
2021-06-15 16:00:59 +0200

    Initial commit on matches

---
+++ totally-legit-horoscopes-api/GenerateStarSignMatches.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using totally_legit_horoscopes_api.DataAccess;
+using totally_legit_horoscopes_api.Models;
+
+namespace totally_legit_horoscopes_api
+{
+    public class GenerateStarSignMatches
+    {
+        private const int NUMBER_OF_MONTHS = 12;
+        private Random random;
+        private IEnumerable<StarSign> starSigns;
+        private User user;
+        private StarSignRepository starSignRepository;
+        private StarSignMatch starSignMatch;
+
+
+        public GenerateStarSignMatches(User user, StarSignRepository starSignRepository)
+        {
+            this.random = new Random();
+            this.user = user;
+            this.starSignRepository = starSignRepository;
+        }
+
+        private async Task<StarSignMatch> GetOrGenerateRandomMatch()
+        {
+            return await CreateStarSignMatch();
+        }
+
+        private async Task<StarSignMatch> CreateStarSignMatch()
+        {
+            this.starSignMatch = new StarSignMatch(user.StarSign);
+            starSignMatch.FriendshipMatch = await GenerateRandomMatchAsync();
+            starSignMatch.CareerMatch = await GenerateRandomMatchAsync();
+            starSignMatch.LoveMatch = await GenerateRandomMatchAsync();
+            return starSignMatch;
+        }
+
+        private bool ValidateRandomMatchAsync(int monthIndex)
+        {
+            if (this.starSignMatch.MainStarSign.StartDate.Month == monthIndex
+                || this.starSignMatch.LoveMatch?.StartDate.Month == monthIndex
+                || this.starSignMatch.CareerMatch?.StartDate.Month == monthIndex
+                || this.starSignMatch.FriendshipMatch?.StartDate.Month == monthIndex)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private async Task<StarSign> GenerateRandomMatchAsync()
+        {
+            if (starSigns == null)
+            {
+                starSigns = await starSignRepository.GetAll();
+            }
+
+            int monthIndex = this.random.Next(NUMBER_OF_MONTHS) + 1;
+            while (!ValidateRandomMatchAsync(monthIndex))
+            {
+                monthIndex = this.random.Next(NUMBER_OF_MONTHS);
+            }
+
+            return starSigns.Where(starSign => starSign.StartDate.Month == (monthIndex)).First();
+        }
+    }
+}
---
+++ totally-legit-horoscopes-api/Models/StarSignMatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace totally_legit_horoscopes_api.Models
+{
+    public class StarSignMatch
+    {
+        public StarSignMatch(StarSign mainStarSign)
+        {
+            this.MainStarSign = mainStarSign;
+        }
+
+        public StarSign MainStarSign { get; set; }
+        public StarSign LoveMatch { get; set; }
+        public StarSign CareerMatch { get; set; }
+        public StarSign FriendshipMatch { get; set; }
+    }
+}
