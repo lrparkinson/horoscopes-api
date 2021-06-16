e6c4016edae369c2a5985236ebe5cc9dc384121e
BBDNET\bbdnet2298 <laurenm@bbd.co.za>
2021-06-16 18:28:25 +0200

    Incorporating new combined abstract noun table into project. Updated repos including get random abstract noun method

---
+++ totally-legit-horoscopes-api/DataAccess/AbstractNounRepository.cs
@@ -0,0 +1,22 @@
+﻿using totally_legit_horoscopes_api.Models;
+using totally_legit_horoscopes_api.Contexts;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace totally_legit_horoscopes_api.DataAccess
+{
+    public class AbstractNounRepository:GenericRepository<AbstractNoun>, IAbstractNounRepository
+    {
+        public AbstractNounRepository(TotallyLegitHoroscopesContext context) : base(context)
+        {
+
+        }
+
+        public async Task<AbstractNoun> GetRandomAbstractNoun(bool shouldGetPositiveNoun)
+        {
+            return await context.AbstractNouns.Where(noun => noun.IsPositive == shouldGetPositiveNoun).OrderBy(x => Guid.NewGuid()).FirstAsync();
+        }
+    }
+}
