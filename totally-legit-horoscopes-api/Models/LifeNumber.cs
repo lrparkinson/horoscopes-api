8bcc79aaef4a9f2bb7878ba41f2cc3eb88bde945
OmerElgoni <78789566+OmerElgoni@users.noreply.github.com>
2021-06-17 09:06:10 +0200

    Update LifeNumber.cs

--- totally-legit-horoscopes-api/Models/LifeNumber.cs
+++ totally-legit-horoscopes-api/Models/LifeNumber.cs
@@ -5,7 +5,7 @@ namespace totally_legit_horoscopes_api.Models
     public class LifeNumber
     {
         [Key]
-        public int lifeNumber { get; set; }
+        public int LifeNumber { get; set; }
         public string Description { get; set; }
     }
 }
