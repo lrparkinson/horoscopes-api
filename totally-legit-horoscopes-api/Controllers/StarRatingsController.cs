c15b2e7450c67c25a576f746a530ca55035be0af
ludevinparkinson@gmail.com <ludevinparkinson@gmail.com>
2021-06-17 08:14:24 +0200

    Re-added Star Ratings Controller

---
+++ totally-legit-horoscopes-api/Controllers/StarRatingsController.cs
@@ -0,0 +1,52 @@
+﻿@@ -0,0 + 1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using totally_legit_horoscopes_api.Contexts;
+using totally_legit_horoscopes_api.Models;
+namespace totally_legit_horoscopes_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StarRatingsController : Controller
+    {
+        private readonly TotallyLegitHoroscopesContext _context;
+        private readonly IMapper _mapper;
+
+        public StarRatingsController(TotallyLegitHoroscopesContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: api/StarRatings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IDictionary<string, int>>> GetUserStarRatings(long id)
+        {
+            User user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(GenerateUserStarRatings(user));
+        }
+
+        private IDictionary<string, int> GenerateUserStarRatings(User user)
+        {
+            IQueryable<StarRatingCategory> starRatingCategories = _context.StarRatingCategories.Distinct();
+            int seed = HashCode.Combine(DateTime.UtcNow.Date, user.StarSign, user.Profession, user.FavoriteDinosaur);
+            Random random = new Random(seed);
+            IDictionary<string, int> ratings = new Dictionary<string, int>();
+
+            starRatingCategories.ToList().ForEach((element) =>
+            {
+                ratings.Add(element.Name, random.Next(0, 6));
+            });
+            return ratings;
+        }
+    }
+}
